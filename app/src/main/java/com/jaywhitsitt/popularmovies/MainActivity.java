f3d33d66f50c8505cd40ac31755950925b013302
Jay Whitsitt <jay.whitsitt@gmail.com>
2019-05-16 13:13:11 -0500

    Change to two-column grid layout

--- app/src/main/java/com/jaywhitsitt/popularmovies/MainActivity.java
+++ app/src/main/java/com/jaywhitsitt/popularmovies/MainActivity.java
@@ -1,9 +1,14 @@
 package com.jaywhitsitt.popularmovies;
 
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,12 +22,12 @@ public class MainActivity extends AppCompatActivity {
 
         mRecyclerView = findViewById(R.id.rv_movies);
 
-        LinearLayoutManager layoutManager
-                = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        GridLayoutManager layoutManager = new GridLayoutManager(this, 2);
         mRecyclerView.setLayoutManager(layoutManager);
         mRecyclerView.setHasFixedSize(true);
 
         mMovieAdapter = new MovieAdapter();
         mRecyclerView.setAdapter(mMovieAdapter);
     }
+
 }

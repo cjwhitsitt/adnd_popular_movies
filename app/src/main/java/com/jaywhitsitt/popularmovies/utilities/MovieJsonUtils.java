6e6f829f8ab5115e9d959771f4350fc0f519439e
Jay Whitsitt <jay.whitsitt@gmail.com>
2019-05-20 21:40:45 -0500

    Move keys to static vars

--- app/src/main/java/com/jaywhitsitt/popularmovies/utilities/MovieJsonUtils.java
+++ app/src/main/java/com/jaywhitsitt/popularmovies/utilities/MovieJsonUtils.java
@@ -11,20 +11,27 @@ import java.sql.Date;
 
 public class MovieJsonUtils {
 
-    // TODO: common variables for json keys
+    private static final String RESULTS_KEY = "results";
+    private static final String ID_KEY = "id";
+    private static final String TITLE_KEY = "title";
+    private static final String POSTER_PATH_KEY = "poster_path";
+    private static final String RUNTIME_KEY = "runtime";
+    private static final String RELEASE_DATE_KEY = "release_date";
+    private static final String VOTE_AVERAGE_KEY = "vote_average";
+    private static final String OVERVIEW_KEY = "overview";
 
     public static MovieBase[] moviesFromJson(String string) {
         MovieBase[] movies = null;
 
         try {
             JSONObject json = new JSONObject(string);
-            JSONArray results = json.getJSONArray("results");
+            JSONArray results = json.getJSONArray(RESULTS_KEY);
             movies = new MovieBase[results.length()];
 
             for (int i = 0; i < results.length(); i++) {
-                int id = results.getJSONObject(i).getInt("id");
-                String title = results.getJSONObject(i).getString("title");
-                String posterPath = results.getJSONObject(i).getString("poster_path");
+                int id = results.getJSONObject(i).getInt(ID_KEY);
+                String title = results.getJSONObject(i).getString(TITLE_KEY);
+                String posterPath = results.getJSONObject(i).getString(POSTER_PATH_KEY);
                 movies[i] = new MovieBase(id, title, posterPath);
             }
 
@@ -40,14 +47,14 @@ public class MovieJsonUtils {
 
         try {
             JSONObject obj = new JSONObject(jsonString);
-            int id = obj.getInt("id");
-            String title = obj.getString("title");
-            String posterPath = obj.getString("poster_path");
-            int runtime = obj.getInt("runtime");
-            String releaseDateString = obj.getString("release_date");
+            int id = obj.getInt(ID_KEY);
+            String title = obj.getString(TITLE_KEY);
+            String posterPath = obj.getString(POSTER_PATH_KEY);
+            int runtime = obj.getInt(RUNTIME_KEY);
+            String releaseDateString = obj.getString(RELEASE_DATE_KEY);
             Date releaseDate = Date.valueOf(releaseDateString);
-            double rating = obj.getDouble("vote_average");
-            String synopsis = obj.getString("overview");
+            double rating = obj.getDouble(VOTE_AVERAGE_KEY);
+            String synopsis = obj.getString(OVERVIEW_KEY);
             movie = new MovieDetail(id, title, posterPath, runtime, releaseDate, rating, synopsis);
 
         } catch (JSONException e) {
